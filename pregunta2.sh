cbced10a8b566c23e2c16f5f21a802ec7f4f2a30
Alvaro <you@example.com>
2020-05-04 18:45:03 +0200

    acabando 2 pregunta

--- pregunta2.sh
+++ pregunta2.sh
@@ -1,2 +1,16 @@
 read -p "introduce primer valor" m
-read -p "introduce segundo valor"n
+read -p "introduce segundo valor" n
+
+total= ((n+total))
+contador=0
+media=0
+
+until [[ $n -eq $m ]]; do
+	total= ((total+$n))
+	((n+1))
+	((contador+1))
+	
+done
+((media=total/acumulador))
+
+echo
